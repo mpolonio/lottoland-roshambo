202027c87e9785887db0a65d7b3c65a5305747ce
Manuel Polonio <manuelpolonioIDB>
2020-05-27 09:05:51 +0200

    Initial commit
    Initial commit with project as generated by Spring Initializr

---
+++ src/main/java/com/dlinkddns/mpolonio/lottolandroshambo/LottolandRoshamboApplication.java
@@ -0,0 +1,13 @@
+package com.dlinkddns.mpolonio.lottolandroshambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottolandRoshamboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottolandRoshamboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dlinkddns/mpolonio/lottolandroshambo/LottolandRoshamboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlinkddns.mpolonio.lottolandroshambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottolandRoshamboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
